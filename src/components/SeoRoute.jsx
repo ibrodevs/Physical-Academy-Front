a5b45881c8e1ca8df933c625575d60f29ced3d71
islam-24-03 <isabhnisa@gmail.com>
2026-03-12 20:42:46 +0600

    finished the task

--- src/components/SeoRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useSeoManager } from '../hooks/useSeoManager';
-
-const SeoRoute = ({ component: Component, pageKey, ...props }) => {
-  useSeoManager(pageKey);
-  return <Component {...props} />;
-};
-
-export default SeoRoute;
